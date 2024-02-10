ea1c14c5b2820771b8b9d0eaae62e83c37d81357
Unknownmaster0 <sagar192002singh@gmail.com>
2024-02-10 10:50:42 +0530

    added problem link

--- palindromic_substrings.cpp
+++ palindromic_substrings.cpp
@@ -1,29 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int check(int l, int r, string s){
+// Problem link -> https://leetcode.com/problems/palindromic-substrings/description/?envType=daily-question&envId=2024-02-10
+
+int check(int l, int r, string s)
+{
 
     int ans = 0;
-    while(l >=0 && r <= s.size()){
-        if(s[l--] == s[r++]) ans ++;
-        else   break;
+    while (l >= 0 && r <= s.size())
+    {
+        if (s[l--] == s[r++])
+            ans++;
+        else
+            break;
     }
     return ans;
 }
 
-int countSubstrings(string s){  // consider s = "saghbhgab"
+int countSubstrings(string s)
+{ // consider s = "saghbhgab"
 
     int n = s.size(), cnt = 0;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cnt += check(i,i,s); // for odd palindrome length
-        cnt += check(i,i+1,s); // for even palindrome length.
+        cnt += check(i, i, s);     // for odd palindrome length
+        cnt += check(i, i + 1, s); // for even palindrome length.
     }
     return cnt;
 }
 
 int main()
 {
-    
+
     return 0;
 }
