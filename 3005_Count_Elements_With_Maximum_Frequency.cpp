d5ba35149ed87ca9b96f81674de3ad24c85681f8
Unknownmaster0 <sagar192002singh@gmail.com>
2024-03-08 19:08:11 +0530

    leetcode potd

---
+++ 3005_Count_Elements_With_Maximum_Frequency.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+
+// t.c = O(N) || s.c = O(N)  [where 'N' is the number of elements in the nums.]
+    int maxFrequencyElements(vector<int>& nums) {
+        int maxFreq = -1, total = 0;
+        unordered_map<int,int> freq;
+// storing the frequency of each element.
+        for(auto it:nums){
+            freq[it] ++;
+            maxFreq = max(maxFreq, freq[it]);
+        }
+// now making the total of the elements having frequency equals to maximum frequency.
+        for(auto it:freq){
+            if(it.second == maxFreq)
+                total += it.second;
+        }
+
+        return total;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
